76aa934ff3a7cff8fb865a4f761c9c28a23dfd8d
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-08-21 17:50:55 -0700

    test

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -179,32 +179,34 @@ KeplerModel.saveChecklist = async (data) => {
         'X',
         'X'
     ];
-    try {
-        params.forEach((param, index) => {
-            console.log(`Index ${index}:`, param, typeof param);
-        });
-        if (params.includes(undefined) || params.includes(null)) {
-            console.error('Array contiene valores undefined o null');
-        }
-        params = params.map(val => val !== undefined && val !== null ? val : '');
+    console.log(params)
+    return { status: true }
+    // try {
+    //     params.forEach((param, index) => {
+    //         console.log(`Index ${index}:`, param, typeof param);
+    //     });
+    //     if (params.includes(undefined) || params.includes(null)) {
+    //         console.error('Array contiene valores undefined o null');
+    //     }
+    //     params = params.map(val => val !== undefined && val !== null ? val : '');
 
-        const query = `INSERT INTO KDS_CHECKLIST (c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
-            c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, 
-            c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40, c41, 
-            c42, c43, c44, c45, c46, c47, c48, c49, c50, c51, c52, c53, c54, 
-            c55, c56, c57, c58, c59, c60, c61, c62, c63, c64, c65, c66, c67,
-            c68, c69, c70, c71, c72, c73, c74, c75, c76, c77, c78, c79, c80, 
-            c81, c82, c83, c84, c85, c86, c87, c88, c89, c90
-        ) VALUES (${params.map(() => '?').join(', ')})
-        `;
-        await sequelize.query(query, {
-            replacements: params,
-        });
-        return { status: true }
-    } catch (error) {
-        console.error('Error al insertar datos:', error);
-        return { status: false, message: error }
-    }
+    //     const query = `INSERT INTO KDS_CHECKLIST (c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
+    //         c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, 
+    //         c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40, c41, 
+    //         c42, c43, c44, c45, c46, c47, c48, c49, c50, c51, c52, c53, c54, 
+    //         c55, c56, c57, c58, c59, c60, c61, c62, c63, c64, c65, c66, c67,
+    //         c68, c69, c70, c71, c72, c73, c74, c75, c76, c77, c78, c79, c80, 
+    //         c81, c82, c83, c84, c85, c86, c87, c88, c89, c90
+    //     ) VALUES (${params.map(() => '?').join(', ')})
+    //     `;
+    //     await sequelize.query(query, {
+    //         replacements: params,
+    //     });
+    //     return { status: true }
+    // } catch (error) {
+    //     console.error('Error al insertar datos:', error);
+    //     return { status: false, message: error }
+    // }
 }
 
 KeplerModel.getFolio = async (data) => {
