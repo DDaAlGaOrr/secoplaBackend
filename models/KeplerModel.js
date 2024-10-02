5a0602ae5cdfd862300c6fcd1309c4bbe552d614
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-10-02 16:50:41 -0700

    x

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -382,7 +382,7 @@ KeplerModel.kdsEventos = async (data) => {
     data.foto_2_entrada.trim(),
     data.foto_3_entrada.trim(),
     data.fecha_hora_validacion.trim(),
-    data.responsable.trim(),
+    data.aprobador.trim(),
     data.estatus_kepler_cardex.trim(),
   ];
   const query = `INSERT INTO kds_eventos(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18) VALUES (${params
