35a8e45fa462271cb46620d032e242f199c9862b
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-10-04 19:32:24 -0700

    ready

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -393,18 +393,18 @@ KeplerModel.kdsEventos = async (data) => {
     data.estatus_kepler_cardex.trim(),
   ];
   console.log(params);
-  // const query = `INSERT INTO kds_eventos(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18) VALUES (${params
-  //   .map(() => "?")
-  //   .join(", ")})`;
-  // try {
-  //   await sequelize.query(query, {
-  //     replacements: params,
-  //   });
+  const query = `INSERT INTO kds_eventos(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18) VALUES (${params
+    .map(() => "?")
+    .join(", ")})`;
+  try {
+    await sequelize.query(query, {
+      replacements: params,
+    });
     return { status: true };
-  // } catch (error) {
-  //   console.error("Error al insertar datos:", error);
-  //   return { status: false, message: error };
-  // }
+  } catch (error) {
+    console.error("Error al insertar datos:", error);
+    return { status: false, message: error };
+  }
 };
 
 KeplerModel.getKdsEventos = async () => {
