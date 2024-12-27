736adcae6da683e987b6fadd89f1da1d38f0a02d
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-12-27 14:28:12 -0700

    fix insert to kds_PersonalRH and kds_puestosRH

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -581,8 +581,24 @@ KeplerModel.insertKdsPersonalRh = async (data) => {
     data.puesto.trim(),
     data.contrasenia.trim(),
     data.domicilio.trim(),
+    data.estadoCivil.trim(),
+    data.direccionActual.trim(),
+    data.nacion.trim(),
+    data.lugarNacimiento.trim(),
+    data.religion.trim(),
+    data.identificacionCiudadana.trim(),
+    data.fechaEmision.trim(),
+    data.lugarEmision.trim(),
+    data.residente.trim(),
+    data.numeroCuentaBancaria.trim(),
+    data.banco.trim(),
+    data.codigoFiscalPersonal.trim(),
+    data.nombreContacto.trim(),
+    data.telefonoContacto.trim(),
+    data.parentescoContacto.trim(),
+    data.staff.trim(),
   ];
-  const query = `INSERT INTO kds_PersonalRH(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15) VALUES (${params
+  const query = `INSERT INTO kds_PersonalRH(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32) VALUES (${params
     .map(() => "?")
     .join(",")})`;
   try {
@@ -613,8 +629,19 @@ KeplerModel.insertKdsPuestosRh = async (data) => {
     data.periodoPago.trim(),
     data.cantidadPagarPeriodo.trim(),
     data.bonos.trim(),
+    data.otrosIngresos.trim(),
+    data.sumaTotal.trim(),
+    data.licenciaManejo.trim(),
+    data.rolTurnos.trim(),
+    data.detalleRolar.trim(),
+    data.examenesMedicos.trim(),
+    data.capacitacionConCliente.trim(),
+    data.detalleCapacitacion.trim(),
+    data.diasLaborales.trim(),
+    data.descripcionPuesto.trim(),
+    data.staff.trim(),
   ];
-  const query = `INSERT INTO kds_puestosRH(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15) VALUES (${params
+  const query = `INSERT INTO kds_puestosRH(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17.c18,c19,c20,c21,c22,c23,c24,c25,c26) VALUES (${params
     .map(() => "?")
     .join(",")})`;
   try {
