d22c390f60b87c3b8b29cc83bb05c5970c90b204
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-12-27 15:09:46 -0700

    some

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -641,6 +641,7 @@ KeplerModel.insertKdsPuestosRh = async (data) => {
     data.descripcionPuesto.trim(),
     data.staff.trim(),
   ];
+  
   const query = `INSERT INTO kds_puestosRH(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17.c18,c19,c20,c21,c22,c23,c24,c25,c26) VALUES (${params
     .map(() => "?")
     .join(",")})`;
@@ -696,3 +697,5 @@ KeplerModel.updateKdsPersonalRh = async (data) => {
 };
 
 module.exports = KeplerModel;
+
+
