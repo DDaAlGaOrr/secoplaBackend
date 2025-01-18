e436b57e15c4e4ffe85e2bfa00de4b5b99e9c5a5
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2025-01-17 20:16:35 -0700

    implements km to update kds_cardex_vehiculos

--- models/KeplerModel.js
+++ models/KeplerModel.js
@@ -369,7 +369,7 @@ KeplerModel.saveKdsKilometrajeGps = async (data) => {
 KeplerModel.updateEstatusCardex = async (data) => {
   console.log(data)
   return await connection.executeQuery(
-    `UPDATE kds_cardex_vehiculos SET c3 = '${data.estatus_cardex}' WHERE c1 = '${data.id}';`
+    `UPDATE kds_cardex_vehiculos SET c3 = '${data.estatus_cardex}', c34 = '${data.km}' WHERE c1 = '${data.id}';`
   );
 };
 
