04534d7763aaa9e5daf38575c54e17885711ee6d
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2025-06-12 11:33:17 -0700

    test

--- routes/sipoc.js
+++ routes/sipoc.js
@@ -4,4 +4,5 @@ const sipoc = require('./../controllers/SipocController')
 
 router.get('/lln',sipoc.getlln)
 router.get('/getClientservices/:client',sipoc.getClientservices)
+
 module.exports = router
