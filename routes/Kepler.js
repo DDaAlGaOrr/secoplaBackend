a71d02ade1a7c85358d3a4608c58e8b11b676645
ErickHernandezTo <erikctoledano@gmail.com>
2026-05-27 01:04:11 -0600

    correción equipo epp

--- routes/Kepler.js
+++ routes/Kepler.js
@@ -35,7 +35,7 @@ router.get("/getKdil/", Kepler.getKdil);
 router.get("/getAll_kds_matriz/", Kepler.getAll_kds_matriz);
 router.get("/getKds_asignacion_EPP/", Kepler.getKds_asignacion_EPP);
 router.get("/getKds_asignacion_Herramienta/", Kepler.getKds_asignacion_Herramienta);
-router.get("/getkds_equipoepp /", Kepler.getkds_equipoepp );
+router.get("/getkds_equipoepp/", Kepler.getkds_equipoepp );
 
 router.post("/auth", Kepler.auth);
 router.post("/getEppsEnAlmacen", upload.none(), Kepler.getEppsEnAlmacen);
